ed855f228fb4551340d2b5165941fe9aa97aac6c
De Rossi Davide <davide.derossi@cz.lan>
2025-10-06 21:54:11 +0200

    remove Dwelling

--- Dwelling/src/Stanza.java
+++
@@ -1,8 +0,0 @@
-public class Stanza {
-    private double with;
-    private double lenth;
-
-    public static double calcolaDim(double with, double lenth) {
-        return with * lenth;
-    }
-}
