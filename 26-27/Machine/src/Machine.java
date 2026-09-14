f548b7d4e34578fc7aa3690f73edee44392921ff
Lupo-byt <derosi.drr05@gmail.com>
2026-09-14 21:06:43 +0200

    Inizio progetto

---
+++ 26-27/Machine/src/Machine.java
@@ -0,0 +1,17 @@
+public class Machine {
+    private int cilindrata;
+    private String colore;
+    private int km;
+
+    public Machine (int cilindrata, String colore, int km) {
+        this.cilindrata = cilindrata;
+        this.colore = colore;
+        this.km = km;
+    }
+
+    public void accellera(){
+
+    }
+
+    public void accendiMotore(){}
+}
