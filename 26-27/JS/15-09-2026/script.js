4dfeaf96fbb588c71163e5d17efee7c7393d211a
Lupo-byt <derosi.drr05@gmail.com>
2026-09-15 21:44:35 +0200

    19/05/2026

---
+++ 26-27/JS/15-09-2026/script.js
@@ -0,0 +1,14 @@
+console.log("Hello, World!");
+
+if(3>5) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log("Ciao")
+}
+
+let s = "Hello World!";
+
